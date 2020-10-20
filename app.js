cb285dca2243a85681590a6a54d984e3e123e92d
codyalmand <codybalmand@gmail.com>
2020-10-20 17:42:48 -0500

    Added engineer and intern questions

--- app.js
+++ app.js
@@ -34,4 +34,52 @@ const questionsForManager = [
     }
 ];
 
+function engineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name:"engineerName",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name:"engineerId",
+            message: "What is the engineer's ID?"
+        },
+        {
+            type: "input",
+            name: "engineerEmail",
+            message: "What is the engineer's email?"
+        },
+        {
+            type: "input",
+            name: "engineerGithub",
+            message: "What is the engineer's GitHub username?"
+        }
+    ])
+};
 
+function intern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "internName",
+            message: "What is the intern's name?"
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "What is the intern's ID?"
+        },
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is the intern's email?"
+        },
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What is the intern's school?"
+        }
+    ])
+};
